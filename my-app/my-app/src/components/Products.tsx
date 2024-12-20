e322a38d44a3c42c7a79ff68795d579d521a60c3
ganeshguggilla <22bk1a7247@stpetershyd.com>
2024-12-20 14:29:49 +0530

    first commit

--- my-app/my-app/src/components/Products.tsx
+++ my-app/my-app/src/components/Products.tsx
@@ -1,41 +1,51 @@
+import React from 'react'
 //import React from 'react'
 
-export default function Products() {
-    const data = [100, 200,300,400,500];
+export default function EmpSal() {
+    const empData = [
+      { name: "Taj Mahal", location: "Agra delhi", country: "India", URL:"https://th-i.thgim.com/public/migration_catalog/article14363426.ece/alternates/FREE_1200/17YT_TAJ_MAHAL"
+      },
+      { name: "Christ the Redeemer ", location: "corcovado mountains ", country: "Brazil", URL:"https://cdn.britannica.com/54/150754-050-5B93A950/statue-Christ-the-Redeemer-Rio-de-Janeiro.jpg?w=300"
+      },
+      { name: "Machu Picchu", location: "urubamba ", country: "south america", URL:"https://i.natgeofe.com/n/819ea774-aa80-435e-af5a-ae56efee7ce3/92491_3x2.jpg"
+
+      },
+      { name: "Great Wall of China", location: "Asia pacific", country: "china", URL:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQqklgyLbTFNpYRnPd_uwCNNmnRI50WEfuemg&s"
+
+      },
+      { name: "Chichén Itzá", location: "Yucatan Mexico", country: "Mexico", URL:"https://image-tc.galaxy.tf/wijpeg-2st4p53xmq8zhzwvckr7xkbp9/30-chichen-itza.jpg"
+ 
+      },
+      { name: "Great Pyramid of Giza", location: "Egypt", country: "Arab" ,
+         URL:"https://cdn.britannica.com/90/99590-050-FF873D7F/pyramid-Great-Sphinx-Khafre-Egypt-Giza.jpg"}];
   
     // () => ()
-   
-    return <div>
-      
-      <table className="table">
-    <thead>
-      <tr>
-        <th scope="col">Name</th>
-        <th scope="col">Email</th>
-        <th scope="col">Company</th>
-        <th scope="col">Salary</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <td>kamusai</td>
-        <td>kamusai@gmail.com</td>
-        <td>IBM</td>
-        <td>45000</td>
-      </tr>
-      <tr>
-        <td>harish</td>
-        <td>harish@gmail.com</td>
-        <td>HP</td>
-        <td>55000</td>
-      </tr>
-      <tr>
-        <td>guna</td>
-        <td>guna@gmail.com</td>
-        <td>TCS</td>
-        <td>65000</td>
-      </tr>
-     </tbody>
-    </table>
-      </div>;
-  }
+  
+    return (
+      <div>
+        <table className="table">
+          <thead>
+            <tr>
+              <th scope="col">Name</th>
+              <th scope="col">location</th>
+              <th scope="col">Country</th>
+              <th scope="col">URL</th>
+            </tr>
+          </thead>
+          <tbody>
+            {empData.map(
+                (x)=>( 
+                <tr>
+                    <td>{x.name}</td>
+                    <td>{x.location}</td>
+                    <td>{x.country}</td>
+                    <td><img src={x.URL} /></td>
+                  </tr>
+                 )
+            )}
+           
+          </tbody>
+        </table>
+      </div>
+);
+}
